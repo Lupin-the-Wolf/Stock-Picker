03fcdb21c33c2e2c1dc8b6e5b0a2f7236769d42d
Lupin <p.bartausky2@gmail.com>
2022-11-15 07:24:25 -0500

    Add first version of stock_picker

---
+++ stock_picker.rb
@@ -0,0 +1,26 @@
+#Goal: To create a method that selects the best time to buy and sell
+
+
+def stock_pick (prices)
+      most_profit = 0
+      buy_day = ""
+      sell_day = ""
+        prices.each_with_index do |buy, buy_index|
+          prices.each_with_index do |sell, sell_index|
+            if sell_index >= buy_index
+              profit = sell - buy
+                if profit > most_profit
+                  most_profit = profit
+                  buy_day = buy_index
+                  sell_day = sell_index
+                end
+              end
+           
+            end
+            
+          end
+          puts buy_day
+          puts sell_day
+          best_buy_sells = [buy_day, sell_day]
+          
+        end
